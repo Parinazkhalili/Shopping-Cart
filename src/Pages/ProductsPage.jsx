5f4494f31544076fa5e3a64cc722f344e97a2a80
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-04-06 15:14:40 +0330

    Rendering the product list

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -1,13 +1,21 @@
 
 
-import { useProducts } from "../context/ProductContext"
+import { useProducts } from "../context/ProductContext" ;
+import Styles from "./ProductsPage.module.css"
 
 function ProductsPage() {
 
   const products = useProducts();
   console.log(products) ;
   return (
-    <div>ProductsPage</div>
+   <div className={StyleSheet.container}>
+    <div className={Styles.products}>
+
+      {!products.length && <p>Loading...</p>}
+      {products.map ((p) => (<p key={p.id}>{p.title}</p>))}
+    </div>
+    <div>sidebar</div>
+   </div>
   )
 }
 
