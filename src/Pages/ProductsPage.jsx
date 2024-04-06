dbc2818c8ea450dfb3fbc536055ed0ae02c9f480
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-04-06 19:15:56 +0330

    Creating a  search section

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -1,15 +1,27 @@
 
-
+import { ImSearch } from "react-icons/im";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext" ;
 import Styles from "./ProductsPage.module.css"
+import { useState } from "react";
 
 function ProductsPage() {
 
   const products = useProducts();
 
+  const [search, setSearch] = useState('')
+  
+  const searchHandler = () => {
+    console.log('search')
+  }
   return (
+    <>
+
+    <div>
+      <input type="text"  placeholder="Search..."  value={search} onChange ={ e => setSearch(e.target.value.toLowerCase().trim())}/>
+      <button  onClick={searchHandler}><ImSearch /></button>
+    </div>
    <div className={Styles.container}>
     <div className={Styles.products}>
 
@@ -19,6 +31,7 @@ function ProductsPage() {
 
     <div>sidebar</div>
    </div>
+   </>
   )
 }
 
