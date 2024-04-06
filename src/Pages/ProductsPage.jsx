b35ae24816a9d4d98d4419dbed3026dc4b1ea3ca
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-04-06 16:33:48 +0330

    Creating card component

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -1,19 +1,21 @@
 
 
+import Card from "../components/Card";
 import { useProducts } from "../context/ProductContext" ;
 import Styles from "./ProductsPage.module.css"
 
 function ProductsPage() {
 
   const products = useProducts();
-  console.log(products) ;
+
   return (
-   <div className={StyleSheet.container}>
+   <div className={Styles.container}>
     <div className={Styles.products}>
 
       {!products.length && <p>Loading...</p>}
-      {products.map ((p) => (<p key={p.id}>{p.title}</p>))}
+      {products.map ((p) =>( <Card  key={p.id} data={p} />))}
     </div>
+
     <div>sidebar</div>
    </div>
   )
