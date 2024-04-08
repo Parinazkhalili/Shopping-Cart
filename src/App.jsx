ac959b469166fb5ef51a0badaf2a2b376b468393
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-04-08 09:32:24 +0330

    Combination of context and useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,24 @@ import DetailsPage from "./Pages/DetailsPage"
 import CheckoutPage from "./Pages/CheckoutPage"
 import PageNoteFound from "./Pages/404"
 import ProductsProvider from "./context/ProductContext"
+import CartProvider from "./context/CartContext"
 
 function App() {
 
 
   return (
-   < ProductsProvider>
+
+    <CartProvider>
+  < ProductsProvider>
   <Routes>
     <Route  path="/"  element={<Navigate to="/products"  replace/>}/>
     <Route  path="/products"  element={<ProductsPage />}/>
     <Route  path="/products/:id"  element={<DetailsPage />}/>
     <Route  path="/checkout"  element={<CheckoutPage />}/>
     <Route  path="/*"  element={<PageNoteFound/>}/>
-
-
   </Routes>
   </ProductsProvider>
+  </CartProvider>
   )
 }
 
