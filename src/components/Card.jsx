30bd7c4dfe572e29ec203a960be9036f1a812a15
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-04-08 10:23:09 +0330

    Creation custom usecart hook

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,9 +5,16 @@ import {TbListDetails} from "react-icons/tb" ;
 import {TbShoppingBagCheck} from "react-icons/tb"
 import { shortenText } from '../helper/helper';
 import styles from "./Card.module.css"
+import { useCart } from '../context/CartContext';
 
 function Card({data}) {
    const {id , title, image, price} = data ;
+   const [state, dispatch] = useCart();
+
+   const clickHandler = () => {
+    dispatch({type: "add" , payload: data})
+   }
+   
   return (
     <div  className={styles.card}>
         <img  src={image} alt={title} />
@@ -15,7 +22,11 @@ function Card({data}) {
         <p>{price}$</p>
         <div className={styles.actions}>
            <Link to={`/products/${id}`}><TbListDetails/></Link>
-          <div><button><TbShoppingBagCheck/></button></div>
+          <div>
+            <button onClick={clickHandler}>
+            <TbShoppingBagCheck/>
+            </button>
+            </div>
         </div>
     </div>
   )
