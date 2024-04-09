0a6d1d0d4de0e8cd0b8049668e16d7cae86f4086
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-04-09 16:18:03 +0330

    Launching the shoppingcart page

--- src/Pages/CheckoutPage.jsx
+++ src/Pages/CheckoutPage.jsx
@@ -1,9 +1,21 @@
 
 import React from 'react'
+import { useCart } from '../context/CartContext'
+import BasketCard from '../components/BasketCard'
 
 function CheckoutPage() {
+  const [state, dispatch] = useCart()
+
+  const clickHandler = (type, payload) => dispatch({type, payload}) ;
   return (
-    <div>CheckoutPage</div>
+    <div>
+      <div>
+        {state.selectedItems.map((product) => ( 
+        <BasketCard  key={product.id}  data={product} clickHandler={clickHandler} />
+ 
+         ))}
+      </div>
+    </div>
   )
 }
 
