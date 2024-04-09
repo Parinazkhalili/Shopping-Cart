42e129d59e805933a406afb7a1ad47da896afd99
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-04-09 11:42:21 +0330

    Handling buttons based on the number of products

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import {TbListDetails} from "react-icons/tb" ;
-import {TbShoppingBagCheck} from "react-icons/tb"
-import { shortenText } from '../helper/helper';
+import {TbShoppingBagCheck} from "react-icons/tb";
+import { MdDeleteOutline } from 'react-icons/md';
+
+import { productQuantity, shortenText } from '../helper/helper';
 import styles from "./Card.module.css"
 import { useCart } from '../context/CartContext';
 
 function Card({data}) {
    const {id , title, image, price} = data ;
    const [state, dispatch] = useCart();
-   console.log(state) ;
-
-   const clickHandler = () => {
-    dispatch({type: "ADD_ITEM" , payload: data})
+   const quantity = productQuantity(state, id);
+   
+   const clickHandler = (type) => {
+    dispatch({type , payload: data})
    }
    
   return (
@@ -22,11 +24,32 @@ function Card({data}) {
         <h3>{shortenText(title)}</h3>
         <p>{price}$</p>
         <div className={styles.actions}>
-           <Link to={`/products/${id}`}><TbListDetails/></Link>
+           <Link to={`/products/${id}`}><TbListDetails/>
+           </Link>
+
           <div>
-            <button onClick={clickHandler}>
+
+            {quantity === 1 && ( <button onClick={() => clickHandler("REMOVE_ITEM")}>
+            <MdDeleteOutline/>
+            </button>)}
+
+            {quantity > 1 && ( <button onClick={() => clickHandler("DECREASE")}>
+              -
+            </button>)}
+
+             {!!quantity && <span>{quantity}</span>}
+
+            {quantity === 0 ? ( <button onClick={() => clickHandler("ADD_ITEM")}>
             <TbShoppingBagCheck/>
-            </button>
+            </button>) : <button onClick={() => clickHandler("INCREASE")}>
+               +
+            </button> }
+
+            
+           
+           
+          
+           
             </div>
         </div>
     </div>
